6f18f795c1a2851e38aa6f526d72224ee6cf20a8
Brandon Dyal <brandondyal@gmail.com>
2021-11-29 12:10:40 -0500

    Change text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to hot reload from compose!
+          Edit <code>src/App.js</code> and save to hot reload from feature!
         </p>
         <a
           className="App-link"
